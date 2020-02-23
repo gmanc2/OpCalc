124374a312e9f412b95dd4e84bb81485af29c57d
Glenn <gwallacec2@gmail.com>
2020-02-23 15:01:10 -0500

    add

--- src/Attackspeed.java
+++ src/Attackspeed.java
@@ -10,16 +10,40 @@ public class Attackspeed {
     double as;
     double seconds = 0;
     int op;
-    System.out.println("Input attack modifier amount:");
-    as = scan.nextDouble();
-    System.out.println("Input opp number:");
-    op = scan.nextInt();
-    if(op == 0) {
-        seconds = 1 / (4 + as);
+    int c;
+    double calc;
+    double out;
+    System.out.println("Input 1 for Seconds per attack 2 for calculating modifier");
+    c = scan.nextInt();
+    if(c == 1) {
+        System.out.println("Input attack modifier amount:");
+        as = scan.nextDouble();
+        System.out.println("Input opp number:");
+        op = scan.nextInt();
+        if (op == 0) {
+            seconds = 1 / (4 + as);
+        } else if (op == 1) {
+            seconds = 1 / (4 * (1 + as));
+        }
+        System.out.println("Seconds per full attack: " + seconds);
     }
-    else if(op == 1) {
-        seconds = 1 / (4 * (1 + as));
+    if(c == 2) {
+        System.out.println("Input Opperator:");
+        c = scan.nextInt();
+        if(c==0) {
+            System.out.println("Input seconds desired");
+            calc = scan.nextDouble();
+            out = 1 / calc - 4;
+            System.out.println("Attack modifier:" + out);
+        }
+        else if (c == 1){
+            System.out.println("Input seconds desired");
+            calc = scan.nextDouble();
+            out = (-4*calc +1)/(4 * calc);
+            System.out.println("Attack modifier:" + out);
+        }
     }
-    System.out.println("Seconds per full attack: " + seconds);
+
+
     }
 }
